d9b582637374b7b3dd3857e44694b74c73dbc0c3
Denis <cruelridicule@gmail.com>
2017-04-27 21:49:46 +0300

    Multiplication

---
+++ Lab2/MultiplicationApp/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultiplicationApp
+{
+    public partial class Form1 : Form
+    {
+        public double a, b, c;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            a = double.Parse(textBox1.Text);
+            b = double.Parse(textBox3.Text);
+            textBox2.Text = (a*b).ToString();
+        }
+    }
+}
