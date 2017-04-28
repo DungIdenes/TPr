6a2ee63b697df44de1459d9d7cd5f774a02d1a9f
Denis <cruelridicule@gmail.com>
2017-04-28 03:51:53 +0300

    Hierarchy

---
+++ Lab6/Hierarchy/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hierarchy
+{
+    public class Test
+    {
+        private int Mark;
+        private string Questions;
+        private int Duration;
+    }
+    public class Vuprobyvannya : Test
+    {
+        private int Mark;
+        private string Questions;
+        private int Duration;
+    }
+    public class Isput : Vuprobyvannya
+    {
+        private int Mark;
+        private string Questions;
+        private int Duration;
+    }
+    public class VupysknuyIsput : Isput
+    {
+        private int Mark;
+        private string Questions;
+        private int Duration;
+    }
+    public class MainClass
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+    }
+    
+}
